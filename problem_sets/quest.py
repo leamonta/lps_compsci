cff6dad335248210a2ae062b52531b81577f46b5
Leamonta Ealy <rc.ealy.leamonta@leadps.org>
2016-11-09 17:48:14 +0000

    Finsh the quest.py game

--- problem_sets/quest.py
+++ problem_sets/quest.py
@@ -1,3 +1,4 @@
+	
 print("Welcome to Leamonta's Quest!!")
 
 print("Enter the name of your character:")
@@ -12,16 +13,26 @@ health = int(raw_input())
 print("Enter luck (1-10):")
 luck = int(raw_input())
 
-if strength +  health + luck > 15: 
-	print("You have give your character too many point! Default values have been assigned, strength: 5, Health: 5, Luck: 5") 
-	strength = 5
-	health = 5
-	luck = 5
+if strength +  health + luck > 15:
+        print("You have give your character too many point! Default values have been assigned, strength: 5, Health: 5, Luck: 5")
+        strength = 5
+        health = 5
+        luck = 5
 
 if strength + health + luck <= 15:
-	print("Your character name is " + name + " and your strength is " + str(strength) + " and your health is " + str(health) + " and your luck is " + str(luck)) 
+        print("Your character name is " + name + " and your strength is " + str(strength) + " and your health is " + str(health) + " and your luck is " + str(luck))
 
 print("You've come to a fork in the road. Do you want to go right or left?")
-choice = raw_input()
-if choice == right and strength + health + luck > 8: 
-	print("
+
+direction = raw_input()
+if direction == "right" and strength <= 8:
+        print("Sorry, your character's strength was too low to get pass the fork and fell in a lake and drowned :(, you lost")
+
+if direction == "right" and strength > 8:
+        print("Yay!!!, you are a strong person to get pass the fork and walked home safely :), you won")
+
+if direction == "left" and health <= 8:
+        print("You tried to get over the fork, but died and while doing it because you health was too low, you lost")
+
+if direction == "left" and health > 8:
+	print("Yayy!!!, Now a bird pick you up and flew you home and be safe because your health was high enough to get over the fork, you won")
